46355fb178d548c6e881bd818b8469c6d8e636b9
mdaipayan <49827916+mdaipayan@users.noreply.github.com>
2026-05-12 12:36:50 +0530

    Create purlin_calc.py

---
+++ utils/purlin_calc.py
@@ -0,0 +1,9 @@
+# Save this inside utils/purlin_calc.py
+
+def run_purlin_design():
+    """
+    Placeholder function for Purlin Design calculations.
+    """
+    # You will eventually add your calculation logic here
+    # For now, we just return a dictionary or dummy string so the import works
+    return "Purlin calculations successful!"
